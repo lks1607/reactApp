d2ab1801e824d4033df67d874fe92609c86752d2
Kyeongseop <seopster@gmail.com>
2023-01-05 19:25:49 +0900

    react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import Button from "./Button";
+// import Button from "./Button";
 import styles from "./App.module.css"
+import {useState,useEffect} from "react";
 
-function App() {
+
+function App(){
+  const [number, setNumber] = useState(0);
+  const [keyword, setValue] = useState("");
+
+  const plus = () => setNumber((prev) => prev + 1);
+  const minus = () => setNumber((prev) => prev - 1);
+  const onChange = (event) => setValue(event.target.value);
+
+  useEffect(() => console.log("I run for count"), [number]);
+  useEffect(() => {
+    if(keyword.length >= 5){
+      return console.log("I Search For " + keyword)
+    }
+  },[keyword]);
+
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome Back!!!</h1>
-      <Button text ="Continue" />
+      <h1>React</h1>
+      <input type="text" placeholder="Search Here!" onChange={onChange} value={keyword}></input>
+      <h2>검색 키워드 : {keyword}</h2>
+      <h2>{number}</h2>
+      <button onClick = {plus} >+1</button>
+      <button onClick = {minus}>-1</button>
     </div>
-  );
+  )
 }
-
 export default App;

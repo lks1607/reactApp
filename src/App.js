4a9b9026c02498487ef8f46d9636949687c87962
Kyeongseop <seopster@gmail.com>
2023-01-06 17:49:34 +0900

    123

--- src/App.js
+++ src/App.js
@@ -4,30 +4,31 @@ import {useState,useEffect} from "react";
 
 
 function App(){
-  const [number, setNumber] = useState(0);
-  const [keyword, setValue] = useState("");
-
-  const plus = () => setNumber((prev) => prev + 1);
-  const minus = () => setNumber((prev) => prev - 1);
-  const onChange = (event) => setValue(event.target.value);
-
-  useEffect(() => console.log("I run for count"), [number]);
-  useEffect(() => {
-    if(keyword.length >= 5){
-      return console.log("I Search For " + keyword)
+  const [toDo,setTodo] = useState("");
+  const [toDos, setTodos] = useState([]);
+  const onChange = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo === ""){
+      return;
     }
-  },[keyword]);
-
-
+    setTodos((currentArray) => ([toDo, ...currentArray]));
+    setTodo("");
+  }
   return (
     <div>
-      <h1>React</h1>
-      <input type="text" placeholder="Search Here!" onChange={onChange} value={keyword}></input>
-      <h2>검색 키워드 : {keyword}</h2>
-      <h2>{number}</h2>
-      <button onClick = {plus} >+1</button>
-      <button onClick = {minus}>-1</button>
+      <h1>My Todo-List({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+      <input onChange={onChange} value={toDo} type="text" placeholder="자신의 목표를 적으세요."></input>
+      <button>전송</button>
+      </form>
+      <hr />
+
+      <ul>
+        {toDos.map((item,index)=>(<li key={index}>{item}</li>))}
+      </ul>
     </div>
+
   )
 }
 export default App;

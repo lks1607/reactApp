af58ac6a17df4c6c3e2cf28a5afbf90a48b2741b
Kyeongseop <seopster@gmail.com>
2023-01-06 18:11:28 +0900

    연습

--- src/App.js
+++ src/App.js
@@ -4,31 +4,46 @@ import {useState,useEffect} from "react";
 
 
 function App(){
-  const [toDo,setTodo] = useState("");
+  const [number, setNumber] = useState(0);
+  const plus = () => setNumber((prev) => prev + 1);
+  const minus = () => setNumber((prev) => prev - 1);
+
+  useEffect(() => console.log('counter click'),[number]);
+
+  const [toDo, setTodo] = useState("");
   const [toDos, setTodos] = useState([]);
+
   const onChange = (event) => setTodo(event.target.value);
   const onSubmit = (event) => {
     event.preventDefault();
-    if(toDo === ""){
-      return;
-    }
-    setTodos((currentArray) => ([toDo, ...currentArray]));
+
+    setTodos((currentArray => [toDo, ...currentArray]));
     setTodo("");
   }
+
   return (
+   <div>
+    <div>
+    <h1>Counter</h1>
+    <h2>{number}</h2>
+    <button onClick = {plus}>+1</button>
+    <button onClick = {minus}>-1</button>
+    </div>
+
+    <hr />
+
     <div>
       <h1>My Todo-List({toDos.length})</h1>
       <form onSubmit={onSubmit}>
-      <input onChange={onChange} value={toDo} type="text" placeholder="자신의 목표를 적으세요."></input>
-      <button>전송</button>
+        <input onChange={onChange} value={toDo} type="text" placeholder="목표를 적어주세요"></input>
+        <button>전송</button>
       </form>
-      <hr />
 
       <ul>
-        {toDos.map((item,index)=>(<li key={index}>{item}</li>))}
+        {toDos.map((item,index) => <li key={index}>{item}</li>)}
       </ul>
     </div>
-
+  </div>
   )
 }
 export default App;

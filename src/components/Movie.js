7dda7524625c5e079c50dbe3bc7a71205ca4f668
Kyeongseop <seopster@gmail.com>
2023-01-13 17:08:07 +0900

    react_app

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,18 +1,22 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styles from "./Movie.module.css"
 
-function Movie({id,coverImg,title,summary,genres}){
+function Movie({id,coverImg,title,summary,genres,year}){
     return (
         <div>
-        <img src ={coverImg} alt={title}/>
-        <h2>
-          <Link to={`/movie/${id}`}>{title}</Link>
-          </h2>
-        <p>{summary}</p>
-        <ul>
-          {genres.map((g) => (<li key={g}>{g}</li>))}
-        </ul>
-        
+          <div className={styles.movie}>
+            <img src ={coverImg} alt={title} className={styles.movieimg}/>
+            <h2 className={styles.movietitle}>
+              <Link to={`/movie/${id}`}>{title}</Link>
+            </h2>
+            <h3 className={styles.movieyear}>개봉연도: {year}</h3>
+            <p>{summary.length > 235 ? `줄거리: ${summary.slice(0,235)}...` : `줄거리: ${summary}`}</p>
+            <ul className={styles.moviegenres}>
+              장르:
+              {genres.map((g) => (<li key={g}>{g}</li>))}
+            </ul>
+          </div>
         </div>)
     
 }
